31c338ac082a666e8e193130edcca2e1f8985e99
nicolasMachut <machut.nicolas@gmail.com>
2014-03-14 17:01:23 +0100

    2 classes test

--- src/test/java/PartieTest.java
+++ src/test/java/PartieTest.java
@@ -1,10 +1,14 @@
+import junit.framework.Assert;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
 /**
  * Created by nicolas on 14/03/14.
  */
 public class PartieTest {
-
+    @Test
     public void partieMediocre()
     {
-        
+        Assert.assertEquals(1, 1);
     }
 }
