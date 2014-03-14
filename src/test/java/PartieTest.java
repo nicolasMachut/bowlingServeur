c8e773025811ca1367eab3e581adfc9829101782
edouardsouan <souan.edouard0@gmail.com>
2014-03-14 17:06:29 +0100

    intégration de la lib festassert intégration de la lib Junit

--- src/test/java/PartieTest.java
+++ src/test/java/PartieTest.java
@@ -1,10 +1,13 @@
 /**
  * Created by nicolas on 14/03/14.
  */
+
+
 public class PartieTest {
 
+    @Test
     public void partieMediocre()
     {
-        Partie partie = new Partie();
+        
     }
 }
